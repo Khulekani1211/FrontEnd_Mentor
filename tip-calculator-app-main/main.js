b7863583cccb44abf2f0a017de1443820f98e3c8
Khulekani Gcaba <khulekanigcaba1211@gmail.com>
2024-06-17 16:58:42 +0200

    Creating a script file and added the disabled button when page loads

---
+++ tip-calculator-app-main/main.js
@@ -0,0 +1,7 @@
+//On load settings
+document.addEventListener("DOMContentLoaded", function (){
+  const resetButton = document.getElementById('reset-button');
+
+  //Disable the reset button
+  resetButton.disabled = true;
+})
