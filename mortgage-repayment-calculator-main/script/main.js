bea068a32a1f8a1a4e07773e8a82a29f80be0f1d
Khulekani1211 <khulekanigcaba1211@gmail.com>
2026-09-12 22:51:59 +0200

    Stating all Element references

---
+++ mortgage-repayment-calculator-main/script/main.js
@@ -0,0 +1,18 @@
+//===============================
+// Element references
+//===============================
+
+const form = document.getElementById('mortgage-form');
+
+const purchasePrice = document.getElementById('loan-amount');
+const paymentTerm = document.getElementById('loan-term');
+const interestRate = document.getElementById('interest-rate');
+
+const withResults = document.getElementById('result');
+const noResults = document.querySelector('.no-result');
+
+const monthlyRepayment = document.getElementById('detail-monthly-amount');
+const totalPayment = document.getElementById('detail-total-payment');
+
+//The "Clear All" button
+const clearAll = document.querySelector('.cal-head a');
